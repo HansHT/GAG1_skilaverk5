1ab7dda9bb74bdeb5a97a5d1aaa56d02583e4c53
HansHT <johannes.htomas@gmail.com>
2024-04-05 10:01:45 +0000

    added rentals

--- find_fd.py
+++ find_fd.py
@@ -1,12 +1,12 @@
 #from itertools import permutations
 
 SQLquery = """
-SELECT 'Shows: %s --> %s' AS FD,
+SELECT 'Matches: %s --> %s' AS FD,
 CASE WHEN COUNT(*)=0 THEN 'MAY HOLD'
 ELSE 'does not hold' END AS VALIDITY
 FROM (
     SELECT X.%s
-    FROM Shows X
+    FROM Matches X
     GROUP BY X.%s
     HAVING COUNT(DISTINCT X.%s) > 1
 );
@@ -16,7 +16,9 @@ def PrintSQL(Att1, Att2):
     print(SQLquery % (Att1, Att2, Att1, Att1, Att2))
 
 #R = ['ID', 'PID', 'SID', 'SN', 'PN', 'MID', 'MN']
-R = ['UID', 'UN', 'SID', 'TI', 'RA', 'IMDB', 'S']
+#R = ['UID', 'UN', 'SID', 'TI', 'RA', 'IMDB', 'S']
+#R = ['PID', 'HID', 'PN', 'S', 'HS', 'HZ', 'HC']
+R = ['MID', 'D', 'HID', 'HN', 'AID', 'AN', 'RID', 'RN', 'H', 'A']
 
 for i in range(len(R)):
     for j in range(len(R)):
